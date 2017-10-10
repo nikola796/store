d03c1737504ad742a7d7858a71d22347ffdb0e20
Vladislav Andreev <Vladislav.Andreev@customs.bg>
2017-10-10 13:00:29 +0300

    Remove redirect

--- app/routes.php
+++ app/routes.php
@@ -1,8 +1,6 @@
 <?php
 
-$router->get('', array('App\Controllers\PagesController', 'index'));
-
-$router->get('home', array('App\Controllers\PagesController', 'home'));
+$router->get('', array('App\Controllers\PagesController', 'home'));
 
 $router->get('{category}', array('App\Controllers\CategoriesController', 'category'));
 
